81086b87af618a3d2a5d87ee14bdf85cee57a190
Asma Begum <asmahinu54@gmail.com>
2025-07-31 22:59:27 +0600

    Update problem2.c

--- problem2.c
+++ problem2.c
@@ -13,8 +13,17 @@ int main()
 
     double d = (m1 * lambda1) / sin(theta1_rad);
     double sin_theta2 = (m2 * lambda2) / d;
-
     
+    if(sin_theta2 > 1.0)
+    {
+       printf("The angle for second order red light is not possible\n");
+    } 
+    else
+    {
+      double theta2_rad = asin(sin_theta2);
+      double theta2_deg = theta2_rad * 180.0 / PI;
+      printf("The angle for second order red light: %.2lf degree\n",theta2_deg);
+    }   
     return 0;
 }
 
